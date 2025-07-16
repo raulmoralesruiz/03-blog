4612cc6078dcc3db2d19b869d6d968ad6905843e
Raúl <raulmdaw@gmail.com>
2025-07-16 14:32:39 +0200

    Imágenes en colecciones

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,22 @@ import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    image: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image(),
+      // .refine((img) => img.width < 1200, {
+      //   message: "La imagen debe ser menor de 1200px",
+      // })
 
-    // relacion
-    author: z.string(),
+      // relacion
+      author: z.string(),
 
-    // relacion
-    tags: z.array(z.string()),
-  }),
+      // relacion
+      tags: z.array(z.string()),
+    }),
 });
 
 export const collections = {
