2a0143c927dc4446f417ab0f24c04c195c23bb0d
Raúl <raulmdaw@gmail.com>
2025-07-17 12:18:57 +0200

    Relaciones en colecciones

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: "content",
@@ -13,13 +13,24 @@ const blogCollection = defineCollection({
       // })
 
       // relacion
-      author: z.string(),
+      // author: z.string(),
+      author: reference("author"),
 
       // relacion
       tags: z.array(z.string()),
     }),
 });
 
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+    }),
+});
+
 export const collections = {
   blog: blogCollection,
+  author: authorCollection,
 };
